6968d3e80de5ba131787abc1e6461d4496cb9d0a
Vlad Zhovtani <zovtanyy@gmail.com>
2026-01-08 13:37:13 +0200

    done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,4 +1,8 @@
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  if (!tabs || tabs.length === 0) {
+    return null;
+  }
+
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
   const currentActiveTabId = activeTab.id;
 
